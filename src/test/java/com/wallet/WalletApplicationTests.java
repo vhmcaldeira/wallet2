e65f59afaadf66470259dbe34b0b1603509e34e8
Victor Hugo Marques Caldeira <victor.marques@db1.com.br>
2021-03-30 18:57:47 -0300

    Configuração inicial com os testes

--- src/test/java/com/wallet/WalletApplicationTests.java
+++ src/test/java/com/wallet/WalletApplicationTests.java
@@ -1,9 +1,16 @@
 package com.wallet;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+
+
 class WalletApplicationTests {
 
 	@Test
